8abb3816b05e0aed41cf522b99a2c6b323df4907
DuyPN <duy.phan@gumiviet.com>
2022-02-25 16:36:03 +0700

    test: fixer catch commit file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\FixerMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -22,6 +23,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\EnsureRequestWantJson::class,
+        FixerMiddleware::class
     ];
 
     /**
